eed9829841a4d0a47bda72e4307da225feb3880d
Nil Portugués Calderó <nilopc@gmail.com>
2015-12-26 16:21:31 +0100

    Create ansistrano.sh

---
+++ ansistrano.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##Ansible
+sudo apt-get -y update
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible
+
+##Install ansistrado roles
+sudo ansible-galaxy install --force carlosbuenosvinos.ansistrano-deploy carlosbuenosvinos.ansistrano-rollback
